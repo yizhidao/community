879e21820acb3a6895110485274220e3bba4b704
易指导 <249557643@qq.com>
2019-11-01 17:11:51 +0800

    first commit

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*
+**
+**@Param: 2019 0928
+**@return: 
+**
+*/
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
